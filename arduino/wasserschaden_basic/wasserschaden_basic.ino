60c9dd9231cfcc858351e7e92b98b8050d20efc3
paul <p4ul@posteo.net>
2018-08-27 00:34:29 +0200

    added ardunio files to repo

---
+++ arduino/wasserschaden_basic/wasserschaden_basic.ino
@@ -0,0 +1,122 @@
+#include "painlessMesh.h"
+#include "settings.cpp" //contains definition of MESH_PREFIX and MESH_PASSWORD
+const int flushTime = 120000;
+const int valve_pin = D1;
+const int status_pin = D2;
+const int opened_value = HIGH;
+const int closed_value = LOW;
+
+String msg_open = "opening valve";
+String msg_close = "closing valve";
+String msg_connected = "con";
+String msg_ping = "pong";
+
+boolean blink_state = false;
+boolean first_time_adjust = true;
+
+boolean is_open = false;
+
+uint32_t bridge;
+
+Scheduler userScheduler;
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+void valve();
+Task closeValveTask(flushTime, 2, &valve);
+Task pingBlink(100, 6, &blink);
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  if(msg == "open"){
+    bridge = from;
+    Serial.println("Received open message");
+    open_valve();
+  }else if(msg == "close"){
+    bridge = from;
+    Serial.println("Received close message");
+    close_valve();
+  }else if(msg == "ping"){
+    mesh.sendSingle(from, msg_ping + "|" + is_open);
+    pingBlink.enable();
+  }
+}
+void blink(){
+  if (pingBlink.isLastIteration()){
+    digitalWrite(status_pin, LOW);
+    blink_state = false;
+    pingBlink.disable();
+    pingBlink.setIterations(6);
+  }else{
+    blink_state = !blink_state;
+    digitalWrite(status_pin, blink_state);
+    Serial.println(blink_state);
+  }
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+    if(first_time_adjust){
+      mesh.sendBroadcast(msg_connected + "|" + is_open);
+      first_time_adjust = false;
+    }
+}
+
+void valve(){
+   if (closeValveTask.isLastIteration()){
+    close_valve();
+    closeValveTask.disable();
+   }
+}
+
+void open_valve(){
+  closeValveTask.setIterations(2);
+  closeValveTask.enable();
+  Serial.println("Opening valve");
+  mesh.sendSingle(bridge, msg_open); 
+  digitalWrite(valve_pin, opened_value);
+  is_open = true;
+}
+
+void close_valve(){
+  Serial.println("Closing valve");
+  mesh.sendSingle(bridge, msg_close); 
+  digitalWrite(valve_pin, closed_value);
+  closeValveTask.disable();
+  is_open = false;
+}
+void setup() {
+  close_valve();
+  Serial.begin(115200);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP ); 
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, 5555 ); //defined in extra file thats not on github
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( closeValveTask );
+  userScheduler.addTask( pingBlink );
+  pinMode(status_pin, OUTPUT);
+  pinMode(valve_pin, OUTPUT);
+  pingBlink.setIterations(2);
+  pingBlink.enable();
+}
+
+void loop() {
+  userScheduler.execute();
+  mesh.update();
+}
---
+++ arduino/wasserschaden_bridge/wasserschaden_bridge.ino
@@ -0,0 +1,137 @@
+#include <Arduino.h>
+#include <painlessMesh.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include "settings.cpp" //contains passwords
+
+#define   MESH_PORT       5555
+
+#define HOSTNAME "MQTT_Bridge"
+
+// Prototypes
+void receivedCallback( const uint32_t &from, const String &msg );
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+void mqttConnect();
+
+char* message_buff;
+
+IPAddress getlocalIP();
+
+IPAddress myIP(0,0,0,0);
+IPAddress mqttBroker(192, 168, 178, 155);
+
+painlessMesh  mesh;
+WiFiClient wifiClient;
+PubSubClient mqttClient(mqttBroker, 1883, mqttCallback, wifiClient);
+
+Scheduler userScheduler;
+Task reconnect(1000, TASK_FOREVER, &mqttConnect);
+
+
+char* convert_for_mqtt(int input){
+  String pubString = String(input);
+  pubString.toCharArray(message_buff, pubString.length()+1); 
+  return message_buff;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 1);
+  mesh.onReceive(&receivedCallback);
+  
+  mesh.onNewConnection([](const uint32_t nodeId) {
+    if(nodeId != 0){
+      String topic = "painlessMesh/from/" + String(nodeId);
+      mqttClient.publish((topic.c_str()), "connected");
+      Serial.printf("New Connection \n", nodeId);
+    }
+  });
+
+  mesh.onDroppedConnection([](const uint32_t nodeId) {
+    if(nodeId != 0){
+      String topic = "painlessMesh/from/" + String(nodeId);
+      mqttClient.publish(topic.c_str(), "dropped");
+      Serial.printf("Dropped Connection %u\n", nodeId);
+    }
+  });
+
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+
+  userScheduler.addTask(reconnect);
+}
+
+void mqttConnect(){
+  if (mqttClient.connect("pmC")) {
+      Serial.println("connected to mqtt");
+      mqttClient.publish("painlessMesh/from/gateway","Ready!");
+      mqttClient.subscribe("painlessMesh/to/#", 0);
+      reconnect.disable();
+  }
+}
+
+void loop() {
+  mesh.update();
+  mqttClient.loop();
+
+  if(myIP != getlocalIP()){
+    myIP = getlocalIP();
+    Serial.println("My IP is " + myIP.toString());
+    mqttConnect();
+  }
+  if(!mqttClient.connected() and !reconnect.isEnabled()) {
+    reconnect.enable();
+    Serial.println("enabled reconnect task");
+  }
+  userScheduler.execute();
+}
+
+void receivedCallback( const uint32_t &from, const String &msg ) {
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+  String topic = "painlessMesh/from/" + String(from);
+  mqttClient.publish(topic.c_str(), msg.c_str());
+}
+
+void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+  char* cleanPayload = (char*)malloc(length+1);
+  payload[length] = '\0';
+  memcpy(cleanPayload, payload, length+1);
+  String msg = String(cleanPayload);
+  free(cleanPayload);
+
+  String targetStr = String(topic).substring(16);
+
+  if(targetStr == "gateway")
+  {
+    if(msg == "getNodes")
+    {
+      //Serial.println( mesh.subConnectionJson().c_str());
+      mqttClient.publish("painlessMesh/from/gateway", mesh.subConnectionJson().c_str());
+    }
+  }
+  else if(targetStr == "broadcast") 
+  {
+    mesh.sendBroadcast(msg);
+  }
+  else
+  {
+    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
+    if(mesh.isConnected(target))
+    {
+      mesh.sendSingle(target, msg);
+    }
+    else
+    {
+      mqttClient.publish("painlessMesh/from/gateway", "Client not connected!");
+    }
+  }
+}
+
+IPAddress getlocalIP() {
+  return IPAddress(mesh.getStationIP());
+}
