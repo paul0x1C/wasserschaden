06b7087af906a2d7db0ebe528fa6e6dad460e412
paul0x1c <p4ul@posteo.net>
2018-10-05 20:59:54 +0200

    implemented status leds

--- arduino/wasserschaden_bridge/wasserschaden_bridge.ino
+++ arduino/wasserschaden_bridge/wasserschaden_bridge.ino
@@ -20,12 +20,14 @@ void receivedCallback( const uint32_t &from, const String &msg );
 void mqttCallback(char* topic, byte* payload, unsigned int length);
 void mqttConnect();
 void mqtt_led_off();
+void mesh_led_off();
 
 char* message_buff;
 
 IPAddress getlocalIP();
 
-IPAddress myIP(0,0,0,0);
+IPAddress no_ip(0, 0, 0, 0);
+IPAddress myIP(0, 0, 0, 0);
 IPAddress mqttBroker(10, 8, 0, 1);
 
 painlessMesh  mesh;
@@ -36,10 +38,11 @@ PubSubClient mqttClient(mqttBroker, 1883, mqttCallback, wifiClient);
 Scheduler userScheduler;
 Task reconnect(1000, TASK_FOREVER, &mqttConnect);
 Task mqtt_blink(50, 1, &mqtt_led_off);
+Task mesh_blink(50, 1, &mesh_led_off);
 
-char* convert_for_mqtt(int input){
+char* convert_for_mqtt(int input) {
   String pubString = String(input);
-  pubString.toCharArray(message_buff, pubString.length()+1); 
+  pubString.toCharArray(message_buff, pubString.length() + 1);
   return message_buff;
 }
 
@@ -58,10 +61,10 @@ void setup() {
   // network (STATION_SSID)
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 1);
   mesh.onReceive(&receivedCallback);
-  
+
   mesh.onNewConnection([](const uint32_t nodeId) {
     digitalWrite(MESH_LED, HIGH);
-    if(nodeId != 0){
+    if (nodeId != 0) {
       String topic = "painlessMesh/from/" + String(nodeId);
       mqttClient.publish((topic.c_str()), "connected");
       Serial.printf("New Connection \n", nodeId);
@@ -69,12 +72,12 @@ void setup() {
   });
 
   mesh.onDroppedConnection([](const uint32_t nodeId) {
-    if(nodeId != 0){
+    if (nodeId != 0) {
       String topic = "painlessMesh/from/" + String(nodeId);
       mqttClient.publish(topic.c_str(), "dropped");
       Serial.printf("Dropped Connection %u\n", nodeId);
     }
-    if(mesh.subConnectionJson().length() < 3){
+    if (mesh.subConnectionJson().length() < 3) {
       digitalWrite(MESH_LED, LOW);
     }
   });
@@ -82,27 +85,36 @@ void setup() {
   mesh.stationManual(STATION_SSID, STATION_PASSWORD);
   mesh.setHostname(HOSTNAME);
 
+  userScheduler.addTask(mesh_blink);
   userScheduler.addTask(mqtt_blink);
   userScheduler.addTask(reconnect);
 }
 
-void mqttConnect(){
+void mqttConnect() {
   if (mqttClient.connect("pmC")) {
-      digitalWrite(MQTT_LED, HIGH);
-      Serial.println("connected to mqtt");
-      mqttClient.publish("painlessMesh/from/gateway","Ready!");
-      mqttClient.subscribe("painlessMesh/to/#", 0);
-      reconnect.disable();
-  }else{
-     digitalWrite(MQTT_LED, LOW);
+    digitalWrite(MQTT_LED, HIGH);
+    Serial.println("connected to mqtt");
+    mqttClient.publish("painlessMesh/from/gateway", "Ready!");
+    mqttClient.subscribe("painlessMesh/to/#", 0);
+    reconnect.disable();
+  } else {
+    digitalWrite(MQTT_LED, LOW);
   }
 }
 
-void mqtt_led_off(){
-   if (mqtt_blink.isLastIteration()){
+void mqtt_led_off() {
+  if (mqtt_blink.isLastIteration()) {
     digitalWrite(MQTT_RX_LED, LOW);
     mqtt_blink.disable();
     mqtt_blink.setIterations(2);
+  }
+}
+
+void mesh_led_off() {
+  if (mesh_blink.isLastIteration()) {
+    digitalWrite(MESH_RX_LED, LOW);
+    mesh_blink.disable();
+    mesh_blink.setIterations(2);
   }
 }
 
@@ -110,13 +122,18 @@ void loop() {
   mesh.update();
   mqttClient.loop();
 
-  if(myIP != getlocalIP()){
-    digitalWrite(WLAN_LED, HIGH);
+  if (myIP != getlocalIP()) {
+    analogWrite(WLAN_LED, 200);
     myIP = getlocalIP();
     Serial.println("My IP is " + myIP.toString());
     mqttConnect();
   }
-  if(!mqttClient.connected() and !reconnect.isEnabled()) {
+  if (myIP == no_ip) {
+    digitalWrite(WLAN_LED, LOW);
+  } else {
+    analogWrite(WLAN_LED, 200);
+  }
+  if (!mqttClient.connected() and !reconnect.isEnabled()) {
     reconnect.enable();
     Serial.println("enabled reconnect task");
   }
@@ -128,35 +145,35 @@ void receivedCallback( const uint32_t &from, const String &msg ) {
   Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
   String topic = "painlessMesh/from/" + String(from);
   mqttClient.publish(topic.c_str(), msg.c_str());
-  digitalWrite(MESH_RX_LED, LOW);
+  mesh_blink.enable();
 }
 
 void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
   digitalWrite(MQTT_RX_LED, HIGH);
-  char* cleanPayload = (char*)malloc(length+1);
+  char* cleanPayload = (char*)malloc(length + 1);
   payload[length] = '\0';
-  memcpy(cleanPayload, payload, length+1);
+  memcpy(cleanPayload, payload, length + 1);
   String msg = String(cleanPayload);
   free(cleanPayload);
 
   String targetStr = String(topic).substring(16);
 
-  if(targetStr == "gateway")
+  if (targetStr == "gateway")
   {
-    if(msg == "getNodes")
+    if (msg == "getNodes")
     {
       //Serial.println( mesh.subConnectionJson().c_str());
       mqttClient.publish("painlessMesh/from/gateway", mesh.subConnectionJson().c_str());
     }
   }
-  else if(targetStr == "broadcast") 
+  else if (targetStr == "broadcast")
   {
     mesh.sendBroadcast(msg);
   }
   else
   {
     uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
-    if(mesh.isConnected(target))
+    if (mesh.isConnected(target))
     {
       mesh.sendSingle(target, msg);
     }
