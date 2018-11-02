60c175c11c82e2787a4afdbd81443d435f1da82b
Paul <p4ul@posteo.net>
2018-11-02 04:55:43 +0100

    change db to localhost

--- db/connection.py
+++ db/connection.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from key import db_pass
 
 # db_url = 'sqlite:///../db_file/production.db'
-db_url = 'mysql://root:%s@172.17.0.3/wasserschaden?charset=utf8' % db_pass
+db_url = 'mysql://wasser:%s@localhost/wasserschaden?charset=utf8' % db_pass
 
 engine = create_engine(db_url, echo=False)
 
