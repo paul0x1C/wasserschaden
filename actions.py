b5fd89e85b809852966e528fe4e1035ba9002a6d
paul0x1c <p4ul@posteo.net>
2019-07-02 21:57:25 +0200

    changed logging level

--- actions.py
+++ actions.py
@@ -6,7 +6,7 @@ from db import models, wrapper
 db_connect = wrapper.db_connect
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def now():
